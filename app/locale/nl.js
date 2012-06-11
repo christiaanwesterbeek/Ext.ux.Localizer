d615c143358afa0580c3d0e1c298e9201d747564
christiaan <christiaan@LTOLPCT93b.pcdm.nl>
2012-06-11 13:35:20 +0200

    Add the other language files

---
+++ app/locale/nl.js
@@ -0,0 +1,11 @@
+﻿[
+{key: "MyButton", value: "Mijn knop"},
+{key: "Boolean", value: "Boolean"},
+{key: "Label", value: "Label"},
+{key: "My Window", value: "Mijn Window"},
+{key: "Browse...", value: "Bladeren..."},
+{key: "Box Label", value: "Box Label"},
+{key: "My Grid Panel", value: "Mijn Grid"},
+{key: "W0500", value: "Waarschuwing 500"},
+{key: "Warning", value: "Waarschuwing"},
+]
